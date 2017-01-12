1d7368be148e85fc4d100d34fff9de14938c9386
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-01-12 11:56:14 -0800

    Update Robot.java
    synch'ing with updated Materbot base

--- src/org/usfirst/frc/team4141/robot/Robot.java
+++ src/org/usfirst/frc/team4141/robot/Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team4141.robot;
 
 
@@ -49,16 +48,38 @@ public class Robot extends MDRobotBase {
 		//A robot is composed of subsystems
 		//A robot will typically have 1 drive system and several other fit to purpose subsystems		
 		//The Drive system is a special subsystem in that it has specific logic handle the speed controllers
-		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
-				.add(MotorPosition.left, new Victor(0))
-				.add(MotorPosition.right, new Victor(1))
+		//We have 2 types of drive systems, tank drive and mecanum drive
+		//uncomment the desired drive system and adjust the motor configuration as needed
+		//Mecanum example :
+		add(new MDDriveSubsystem(this, "driveSystem", Type.MecanumDrive)
+				.add(MotorPosition.frontRight, new Victor(0))
+				.add(MotorPosition.rearRight, new Victor(1))
+				.add(MotorPosition.frontLeft, new Victor(2))
+				.add(MotorPosition.rearLeft, new Victor(3))
 				.add("accelerometer", new MD_BuiltInAccelerometer())
 				.add("IMU", new MD_IMU())
-				.add("a", new DoubleConfigSetting(0.0, 1.0, 0.25))
-				.add("b", new DoubleConfigSetting(0.0, 1.0, 0.4))
-				.add("c", new DoubleConfigSetting(0.0, 1.0, 1.0))
 				.configure()
 		);	
+		
+		//TankDrive with 2 motors example:
+//		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
+//				.add(MotorPosition.right, new Victor(0))
+//				.add(MotorPosition.left, new Victor(1))
+//				.add("accelerometer", new MD_BuiltInAccelerometer())
+//				.add("IMU", new MD_IMU())
+//				.configure()
+//		);	
+		//TankDrive with 4 motors example:
+//		add(new MDDriveSubsystem(this, "driveSystem", Type.TankDrive)
+//				.add(MotorPosition.frontRight, new Victor(0))
+//				.add(MotorPosition.rearRight, new Victor(1))
+//				.add(MotorPosition.frontLeft, new Victor(2))
+//				.add(MotorPosition.rearLeft, new Victor(3))
+//				.add("accelerometer", new MD_BuiltInAccelerometer())
+//				.add("IMU", new MD_IMU())
+//				.configure()
+//		);	
+		
 
 	}
 
