bd360f46b0d27fe8e0c2974a51f124884da65a0d
YaBoiPhi <trand.19@stu.materdei.org>
2017-03-07 18:02:32 -0800

    mapped buttons for xbox, logitech, and joystick for competition

--- src/org/usfirst/frc/team4141/robot/OI.java
+++ src/org/usfirst/frc/team4141/robot/OI.java
@@ -70,31 +70,43 @@ public class OI extends OIBase{
 /*
 			// Controller Config: Xbox
 			// -------------------------------------------------
-			.whenPressed("X",3,new MDMoveCommand(getRobot(),"left command",Direction.left))
-			.whenPressed("A",1,new MDMoveCommand(getRobot(),"reverse command",Direction.reverse))
-			.whenPressed("B",2,new OpenDoorCommand(getRobot(),"Open Door Command"))
-			.whenPressed("B",2,new MDMoveCommand(getRobot(),"right command",Direction.right))
-			.whenPressed("A",1,new MDMoveCommand(getRobot(),"forward command",Direction.forward))
-			.whenPressed("LeftBumper",5,new ExtendCommand(getRobot(),"ExtendCommand"))
-			.whenPressed("RightBumper",6,new RetractCommand(getRobot(),"RetractCommand"))
-			.whenPressed("LeftBumper",5,new MDPrintCommand(getRobot(),"LB","Left Bumper Pressed"))
-			.whenPressed("RightBumper",6,new MDPrintCommand(getRobot(),"RB","Right Bumper Pressed"))
-			.whenPressed("RightBumper",6,new MDPrintCommand(getRobot(),"RB","Right Bumper Pressed"))
-			.whenPressed("Y",4,new ToggleOrientationCommand(getRobot(),"Flip"))
+			.whenPressed("A",1,new UnjamBallsystemCommand(getRobot(),"UnjamBallsystemCommand"))
+			.whenPressed("B",2,new ShiftToggleCommand(getRobot(), "ShiftToggle"))
+			.whenPressed("X",3,new ToggleOrientationCommand(getRobot(), "ToggleOrientationCommand"))
+			.whenPressed("Y",4,new UnjamShootCommand(getRobot(),"UnjamShootCommand"))
+			.whileHeld("LB",5,new RopeRiseCommand(getRobot(), "RopeRiseCommand"))
+			.whileHeld("RB",6,new ShootCommand(getRobot(), "ShootCommand")
+			.whenPressed("start",8,new StopShootSystemCommand(getRobot(),"StopShootSystemCommand"))
+			.configure()
+			);
 */
+/*				
+			//Controller Config: Logitech
+			// --------------------------------------------------
+				.whenPressed("A",2,new UnjamBallsystemCommand(getRobot(),"UnjamBallsystemCommand"))
+				.whenPressed("B",3,new ShiftToggleCommand(getRobot(), "ShiftToggle"))
+				.whenPressed("X",1,new ToggleOrientationCommand(getRobot(), "ToggleOrientationCommand"))
+				.whenPressed("Y",4,new UnjamShootCommand(getRobot(),"UnjamShootCommand"))
+				.whileHeld("LB",5,new RopeRiseCommand(getRobot(), "RopeRiseCommand"))
+				.whileHeld("RB",6,new ShootCommand(getRobot(), "ShootCommand")
+				.whenPressed("start",10,new StopShootSystemCommand(getRobot(),"StopShootSystemCommand"))
+				.configure());
+			*/
+			
 				
 			// Joystick Config: EXTREME 360 Pro
 			// -------------------------------------------------
 			.whileHeld("trigger",1,new ShootCommand(getRobot(), "ShootCommand"))
-			.whenPressed("SideButton",2,new ToggleOrientationCommand(getRobot(), "ToggleOrientationCommand"))
+			.whileHeld("sideButton",2,new RopeRiseCommand(getRobot(), "RopeRiseCommand"))
 			.whenPressed("Button3",3,new ShiftToggleCommand(getRobot(), "ShiftToggle"))
-			.whileHeld("Button4",4,new TalonDriveCommand(getRobot(), "TalonCommand"))
+			.whenPressed("Button4",4,new UnjamShootCommand(getRobot(),"UnjamShootCommand"))
+			.whenPressed("Button5",5,new ToggleOrientationCommand(getRobot(), "ToggleOrientationCommand"))
+			.whenPressed("Button6",6,new UnjamBallsystemCommand(getRobot(),"UnjamBallsystemCommand"))
 			.whenPressed("Button8",8,new StopShootSystemCommand(getRobot(),"StopShootSystemCommand"))
-			.whenPressed("Button9",9,new UnjamShootCommand(getRobot(),"UnjamShootCommand"))
-			.whenPressed("Button10",10,new UnjamBallsystemCommand(getRobot(),"UnjamBallsystemCommand"))
-			.whileHeld("Button11",11,new RopeRiseCommand(getRobot(), "RopeRiseCommand"))
-			.whenPressed("Button12",12,new RumbleCommand(getRobot(), "RumbleCommand"))
-
+			
+			
+//			.whenPressed("Button12",12,new RumbleCommand(getRobot(), "RumbleCommand"))
+//			.whileHeld("Button4",4,new TalonDriveCommand(getRobot(), "TalonCommand"))
 //			.whenPressed("trigger",1,new StopBallsystemCommand(getRobot(), "StopBallsystemCommand"))			
 //		    .whenPressed("Button7",7,new StopBallsystemCommand(getRobot(), "CollectCommand"))			
 //			.whenPressed("Trigger",1,new ShootCommand(getRobot(), "ShootCommand"))
@@ -102,9 +114,8 @@ public class OI extends OIBase{
 //			.whenPressed("Button3",3,new OpenDoorCommand(getRobot(), "OpenDoorCommand"))			
 //			.whenPressed("Button6",6,new MDMoveCommand(getRobot(),"forward command",Direction.forward))			
 //			.whenPressed("Button7",7,new ShootCommand(getRobot(),"ShootCommand"))	
-			
-			.configure()
-		);
+
+			.configure());
 
 		//Configure the RioHID here
 		// Uncomment the following to attach a command to the user button on the RoboRio
